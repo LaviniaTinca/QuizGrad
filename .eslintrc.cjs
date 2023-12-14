47612ce261ce22881da4e234c892186af2e117ad
Lavinia Tinca <laviniaanamariatinca@gmail.com>
2023-12-14 19:19:34 +0200

    added shadcn components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,71 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "airbnb",
+    "airbnb-typescript",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "node_modules", "*.js"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  settings: {
+    "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+  rules: {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/function-component-definition": [
+      "error",
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": ["error"],
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    "no-console": ["error"],
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import { twMerge } from "tailwind-merge";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div
-      className={`bg-gray-100 h-screen flex flex-col items-center justify-center gap-y-14 px-10 ${twMerge(
+      className={` h-screen flex flex-col items-center justify-center gap-y-14 px-10 ${twMerge(
         "lg:px-18",
         "lg:px-20"
       )}`}
     >
-      <p className="text-6xl lg:text-8xl text-red-900 font-bold text-center">
+      <h1 className="text-6xl lg:text-8xl text-qyellow font-bold text-center">
         Quiz Project
-      </p>
-      <p className="text-red-200">something</p>
+      </h1>
+      <Button
+        variant="type1"
+        onClick={() => {
+          alert("Welcome to our Quiz app!");
+        }}
+      >
+        Welcome
+      </Button>
+      <Button
+        variant="type2"
+        onClick={() => {
+          alert("Bienvenue donc notre quiz app!");
+        }}
+      >
+        Bienvenue
+      </Button>
     </div>
   );
 }
